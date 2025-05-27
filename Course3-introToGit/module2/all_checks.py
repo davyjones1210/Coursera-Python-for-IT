fbc12a5a9320b361379f18664c2e4ce15c26a75f
Kunal Dekhane <kunal.dekhane@gmail.com>
2025-05-26 22:13:30 -0400

    Adding minor changes to all_checks.py

--- Course3-introToGit/module2/all_checks.py
+++ Course3-introToGit/module2/all_checks.py
@@ -9,12 +9,12 @@ def check_reboot():
     return os.path.exists("/run/reboot-required")
 
 def check_disk_full(disk, mmin_absolute, min_percent):  
-  """Returns True if there isn’t enough disk space, False otherwise."""  
-  du = shutil.disk_usage(disk)  
-  # Calculate the percentage of free space
+    """Returns True if there isn’t enough disk space, False otherwise."""  
+    du = shutil.disk_usage(disk)  
+    # Calculate the percentage of free space
   
-  percent_free = 100 * du.free / du.total  
-  # Calculate how many free gigabytes
+    percent_free = 100 * du.free / du.total  
+    # Calculate how many free gigabytes
 
 def main():
     if check_reboot():
